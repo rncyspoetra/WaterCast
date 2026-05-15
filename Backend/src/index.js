889b10113ff371005c3eb6072f031d70e0b0c02f
Rncy Spoetra <bagasadilmg123@gmail.com>
2026-05-15 20:31:55 +0700

    feat: add PWA icons, screenshots, and update frontend config

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,10 +14,12 @@ import { errorHandler } from "./utils/errorHandler.js";
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(cookieParser());
--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,15 +1,112 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg"],
+      manifest: {
+        name: "Watercast App",
+        short_name: "App",
+        description:
+          "Aplikasi penjualan air minum galon isi ulang untuk membantu pengelolaan transaksi, data pelanggan, dan monitoring penjualan secara mudah dan efisien.",
+        background_color: "#ffffff",
+        display: "standalone",
+        screenshots: [
+          {
+            src: "/screenshots/desktop/Watercast_001.png",
+            sizes: "1920x1043",
+            type: "image/png",
+            form_factor: "wide",
+          },
+          {
+            src: "/screenshots/desktop/Watercast_002.png",
+            sizes: "1920x1043",
+            type: "image/png",
+            form_factor: "wide",
+          },
+          {
+            src: "/screenshots/desktop/Watercast_003.png",
+            sizes: "1920x1043",
+            type: "image/png",
+            form_factor: "wide",
+          },
+          {
+            src: "/screenshots/desktop/Watercast_004.png",
+            sizes: "1920x1043",
+            type: "image/png",
+            form_factor: "wide",
+          },
+          {
+            src: "/screenshots/mobile/Watercast_001.png",
+            sizes: "390x844",
+            type: "image/png",
+            form_factor: "narrow",
+          },
+          {
+            src: "/screenshots/mobile/Watercast_002.png",
+            sizes: "390x844",
+            type: "image/png",
+            form_factor: "narrow",
+          },
+          {
+            src: "/screenshots/mobile/Watercast_003.png",
+            sizes: "390x844",
+            type: "image/png",
+            form_factor: "narrow",
+          },
+          {
+            src: "/screenshots/mobile/Watercast_004.png",
+            sizes: "390x844",
+            type: "image/png",
+            form_factor: "narrow",
+          },
+          {
+            src: "/screenshots/mobile/Watercast_005.png",
+            sizes: "390x844",
+            type: "image/png",
+            form_factor: "narrow",
+          },
+        ],
+        icons: [
+          {
+            src: "/icon-x144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/maskable_icon_x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/maskable_icon_x384.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/maskable_icon_x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
-
+});
